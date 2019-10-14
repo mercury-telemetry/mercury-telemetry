dd7415d4819d8db9f36c116b50cc3bcb5c2529fa
Tara Tran <taratptran@gmail.com>
2019-10-14 10:14:06 -0400

    added index and about us page

---
+++ env/bin/django-admin.py
@@ -0,0 +1,6 @@
+#!/Users/taratran/fall2019-cs-gy-6063-team-moonsurvivors/env/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.1','django-admin.py'
+__requires__ = 'Django==3.1'
+__import__('pkg_resources').require('Django==3.1')
+__file__ = '/Users/taratran/fall2019-cs-gy-6063-team-moonsurvivors/django/django/bin/django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
