ae6f225f8c1707beba7d20dfed72e2ec04573274
de846 <de846@nyu.edu>
2019-10-14 21:43:43 -0400

    Update installed apps

--- nyutelemetry/settings.py
+++ nyutelemetry/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app.apps.HowdyConfig'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import django_heroku
 django_heroku.settings(locals())
-
--- nyutelemetry/urls.py
+++ nyutelemetry/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-        url(r'^admin/', admin.site.urls),
-        url(r'^', include('app.urls')), 
-               ]
+        path('^admin/', admin.site.urls),
+        path('^', include('app.urls'))
+        ]
