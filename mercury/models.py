77d917c615eea5cffaae68c9b3b74bab2f7dcf3c
de846 <de846@nyu.edu>
2019-10-16 00:29:23 -0400

    Fix URL and add EngineTemp model

--- mercury/models.py
+++ mercury/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class EngineTemperatureSensor(models.Model):
+    format = "celsius"
+    temperature = models.IntegerField()
+    timestamp = models.DateTimeField("date measurement was taken")
+    now = timezone.now()
+
+    def __str__(self):
+        return self.temperature
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path('/', include('mercury.urls'))
+        path('', include('mercury.urls'))
 ]
