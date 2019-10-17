ce3bfac2c025e0a90f4cc002cbcb89127dee2aff
de846 <de846@nyu.edu>
2019-10-17 13:49:17 -0400

    Run black

--- mercury/views.py
+++ mercury/views.py
@@ -1,11 +1,12 @@
- # howdy/views.py
+# howdy/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        return render(request, "index.html", context=None)
+
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
