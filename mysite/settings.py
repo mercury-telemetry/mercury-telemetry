d9bf3e1aa9dbd922a29adf737e43478ac7b13e31
de846 <de846@nyu.edu>
2019-10-25 22:21:59 -0400

    Set STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-STATIC_ROOT = ""
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 # django_heroku.settings(locals())
