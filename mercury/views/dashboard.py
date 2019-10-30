c4de7684966f98a28e96756e271bd99fe16e257c
de846 <de846@nyu.edu>
2019-10-30 00:19:30 -0400

    Add refresh and desc ordering

--- mercury/views/dashboard.py
+++ mercury/views/dashboard.py
@@ -8,7 +8,7 @@ class DashboardView(TemplateView):
     template_name = "dashboard.html"
 
     def get(self, request, *args, **kwargs):
-        all_data = SimulatedData.objects.all()
+        all_data = SimulatedData.objects.all().order_by("-created_at")
         return render(request, self.template_name, {"all_data": all_data})
 
 
