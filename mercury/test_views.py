d250c440f143c1716f8f03b3b1d7ddd39592ca95
de846 <de846@nyu.edu>
2019-10-30 08:33:01 -0400

    Add tests for views
    Add staticfiles to .gitignore
    Fix typo in asset name in dashboard-live

---
+++ mercury/test_views.py
@@ -0,0 +1,20 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class TestViews(TestCase):
+
+    def test_dashboard(self):
+        client = Client()
+        response = client.get(reverse('mercury:dashboard'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_dashboard_live(self):
+        client = Client()
+        response = client.get(reverse('mercury:dashboard-live'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_simulator(self):
+        client = Client()
+        response = client.get(reverse('mercury:simulator'))
+        self.assertEqual(response.status_code, 200)
