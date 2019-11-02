5403ff1d7a3968e2ec3386d8140857cdc89e8132
rajeev <rajeev.ilavala@gmail.com>
2019-11-02 04:36:17 -0400

    user story #95 Continous Submission

--- mercury/forms.py
+++ mercury/forms.py
@@ -10,107 +10,111 @@ class RangeInput(NumberInput):
 
 
 class SimulatorForm(forms.ModelForm):
-    name = forms.CharField(max_length=150, strip=True)
-    owner = forms.CharField(max_length=150, strip=True)
+    # name = forms.CharField(max_length=150, strip=True)
+    # owner = forms.CharField(max_length=150, strip=True)
 
-    temperature = forms.FloatField()
+    # temperature = forms.FloatField()
 
-    acceleration_x = forms.FloatField()
-    acceleration_y = forms.FloatField()
-    acceleration_z = forms.FloatField()
+    # acceleration_x = forms.FloatField()
+    # acceleration_y = forms.FloatField()
+    # acceleration_z = forms.FloatField()
 
-    wheel_speed_fr = forms.FloatField()
-    wheel_speed_fl = forms.FloatField()
-    wheel_speed_br = forms.FloatField()
-    wheel_speed_bl = forms.FloatField()
+    # wheel_speed_fr = forms.FloatField()
+    # wheel_speed_fl = forms.FloatField()
+    # wheel_speed_br = forms.FloatField()
+    # wheel_speed_bl = forms.FloatField()
 
-    suspension_fr = forms.FloatField()
-    suspension_fl = forms.FloatField()
-    suspension_br = forms.FloatField()
-    suspension_bl = forms.FloatField()
+    # suspension_fr = forms.FloatField()
+    # suspension_fl = forms.FloatField()
+    # suspension_br = forms.FloatField()
+    # suspension_bl = forms.FloatField()
 
-    # Fuel Supply Panel
-    initial_fuel = forms.FloatField()
-    fuel_decrease_rate = forms.FloatField()
+    # # Fuel Supply Panel
+    # initial_fuel = forms.FloatField()
+    # fuel_decrease_rate = forms.FloatField()
 
-    # Oil Supply/Level Panel
-    initial_oil = forms.FloatField()
-    oil_decrease_rate = forms.FloatField()
+    # # Oil Supply/Level Panel
+    # initial_oil = forms.FloatField()
+    # oil_decrease_rate = forms.FloatField()
 
-    def __init__(self, *args, **kwargs):
-        super(SimulatorForm, self).__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     super(SimulatorForm, self).__init__(*args, **kwargs)
 
-        # acceleration_x
-        self.fields[
-            "acceleration_x"
-        ].help_text = "Choose an acceleration values in m/s2"
-        self.fields["acceleration_x"].label = "Acceleration in the X direction"
-        # acceleration_y
-        self.fields[
-            "acceleration_y"
-        ].help_text = "Choose an acceleration values in m/s2"
-        self.fields["acceleration_y"].label = "Acceleration in the Y direction"
-        # acceleration_z
-        self.fields[
-            "acceleration_z"
-        ].help_text = "Choose an acceleration values in m/s2"
-        self.fields["acceleration_z"].label = "Acceleration in the Z direction"
+    #     # acceleration_x
+    #     self.fields[
+    #         "acceleration_x"
+    #     ].help_text = "Choose an acceleration values in m/s2"
+    #     self.fields["acceleration_x"].label = "Acceleration in the X direction"
+    #     # acceleration_y
+    #     self.fields[
+    #         "acceleration_y"
+    #     ].help_text = "Choose an acceleration values in m/s2"
+    #     self.fields["acceleration_y"].label = "Acceleration in the Y direction"
+    #     # acceleration_z
+    #     self.fields[
+    #         "acceleration_z"
+    #     ].help_text = "Choose an acceleration values in m/s2"
+    #     self.fields["acceleration_z"].label = "Acceleration in the Z direction"
 
-        # wheel_speed_fr
-        self.fields["wheel_speed_fr"].help_text = "Enter Wheel Speed in m/s"
-        self.fields["wheel_speed_fr"].label = "Wheel Speed for FR (Front Right)"
-        # wheel_speed_fl
-        self.fields["wheel_speed_fl"].help_text = "Enter Wheel Speed in m/s"
-        self.fields["wheel_speed_fl"].label = "Wheel Speed for FL (Front Left)"
-        # wheel_speed_bl
-        self.fields["wheel_speed_bl"].help_text = "Enter Wheel Speed in m/s"
-        self.fields["wheel_speed_bl"].label = "Wheel Speed for BL (Back Left)"
-        # wheel_speed_fl
-        self.fields["wheel_speed_br"].help_text = "Enter Wheel Speed in m/s"
-        self.fields["wheel_speed_br"].label = "Wheel Speed for BR (Back Right)"
+    #     # wheel_speed_fr
+    #     self.fields["wheel_speed_fr"].help_text = "Enter Wheel Speed in m/s"
+    #     self.fields["wheel_speed_fr"].label = "Wheel Speed for FR (Front Right)"
+    #     # wheel_speed_fl
+    #     self.fields["wheel_speed_fl"].help_text = "Enter Wheel Speed in m/s"
+    #     self.fields["wheel_speed_fl"].label = "Wheel Speed for FL (Front Left)"
+    #     # wheel_speed_bl
+    #     self.fields["wheel_speed_bl"].help_text = "Enter Wheel Speed in m/s"
+    #     self.fields["wheel_speed_bl"].label = "Wheel Speed for BL (Back Left)"
+    #     # wheel_speed_fl
+    #     self.fields["wheel_speed_br"].help_text = "Enter Wheel Speed in m/s"
+    #     self.fields["wheel_speed_br"].label = "Wheel Speed for BR (Back Right)"
 
-        # suspension_fr
-        self.fields["suspension_fr"].help_text = "Enter Suspension/Compression in cm"
-        self.fields["suspension_fr"].label = "Suspension/Compression for FR"
-        # suspension_fl
-        self.fields["suspension_fl"].help_text = "Enter Suspension/Compression in cm"
-        self.fields["suspension_fl"].label = "Suspension/Compression for FL"
-        # suspension_bl
-        self.fields["suspension_bl"].help_text = "Enter Suspension/Compression in cm"
-        self.fields["suspension_bl"].label = "Suspension/Compression for BL"
-        # suspension_br
-        self.fields["suspension_br"].help_text = "Enter Suspension/Compression in cm"
-        self.fields["suspension_br"].label = "Suspension/Compression for BR"
+    #     # suspension_fr
+    #     self.fields["suspension_fr"].help_text = "Enter Suspension/Compression in cm"
+    #     self.fields["suspension_fr"].label = "Suspension/Compression for FR"
+    #     # suspension_fl
+    #     self.fields["suspension_fl"].help_text = "Enter Suspension/Compression in cm"
+    #     self.fields["suspension_fl"].label = "Suspension/Compression for FL"
+    #     # suspension_bl
+    #     self.fields["suspension_bl"].help_text = "Enter Suspension/Compression in cm"
+    #     self.fields["suspension_bl"].label = "Suspension/Compression for BL"
+    #     # suspension_br
+    #     self.fields["suspension_br"].help_text = "Enter Suspension/Compression in cm"
+    #     self.fields["suspension_br"].label = "Suspension/Compression for BR"
 
-        # initial_fuel
-        self.fields["initial_fuel"].help_text = "Enter fuel amount in gallons"
-        self.fields["initial_fuel"].label = "Initial Fuel Supply"
+    #     # initial_fuel
+    #     self.fields["initial_fuel"].help_text = "Enter fuel amount in gallons"
+    #     self.fields["initial_fuel"].label = "Initial Fuel Supply"
 
-        # fuel_decrease_rate
-        self.fields[
-            "fuel_decrease_rate"
-        ].help_text = "Choose from 0 to 1 (Steps of 0.1)"
-        self.fields["fuel_decrease_rate"].label = "Fuel Decrease Rate"
-        self.fields["fuel_decrease_rate"].widget = RangeInput(
-            attrs={"max": 1, "min": 0, "step": 0.1}
-        )
+    #     # fuel_decrease_rate
+    #     self.fields[
+    #         "fuel_decrease_rate"
+    #     ].help_text = "Choose from 0 to 1 (Steps of 0.1)"
+    #     self.fields["fuel_decrease_rate"].label = "Fuel Decrease Rate"
+    #     self.fields["fuel_decrease_rate"].widget = RangeInput(
+    #         attrs={"max": 1, "min": 0, "step": 0.1}
+    #     )
 
-        # initial_oil
-        self.fields["initial_oil"].help_text = "Enter fuel amount"
-        self.fields["initial_oil"].label = "Initial Oil Supply"
+    #     # initial_oil
+    #     self.fields["initial_oil"].help_text = "Enter fuel amount"
+    #     self.fields["initial_oil"].label = "Initial Oil Supply"
 
-        # fuel_decrease_rate
-        self.fields["oil_decrease_rate"].help_text = "Choose from 0 to 1 (Steps of 0.1)"
-        self.fields["oil_decrease_rate"].label = "Oil Decrease Rate"
-        self.fields["oil_decrease_rate"].widget = RangeInput(
-            attrs={"max": 1, "min": 0, "step": 0.1}
-        )
+    #     # fuel_decrease_rate
+    #     self.fields["oil_decrease_rate"].help_text = "Choose from 0 to 1 (Steps of 0.1)"
+    #     self.fields["oil_decrease_rate"].label = "Oil Decrease Rate"
+    #     self.fields["oil_decrease_rate"].widget = RangeInput(
+    #         attrs={"max": 1, "min": 0, "step": 0.1}
+    #     )
 
+
+# Created by: Rajeev
+# User story: #95 Continuous Submission for simulator UI
+# begin
     class Meta:
         model = SimulatedData
         # fields = "__all__"
 
-        widgets = {"initial_fuel": RangeInput(attrs={"max": 1, "min": 0, "step": 0.1})}
+        #widgets = {"fuel_decrease_rate": RangeInput(attrs={"max": 1, "min": 0, "step": 0.1})}
 
         fields = [
             "name",
@@ -131,9 +135,87 @@ class SimulatorForm(forms.ModelForm):
             "fuel_decrease_rate",
             "initial_oil",
             "oil_decrease_rate",
-            "created_at",
+            "created_at"
         ]
+        widgets = {
+            'name': forms.TextInput(
+                attrs={'id': 'post-name', 'required': True, 'placeholder': 'Enter Name'}
+            ),
 
+            'owner': forms.TextInput(
+                attrs={'id': 'post-owner', 'required': True, 'placeholder': 'Enter Name of the owner'}
+            ),
+
+            'temperature': forms.NumberInput(
+                attrs={'id': 'post-temperature', 'required': True, 'placeholder': 'Enter temperature value'}
+            ),
+
+            'acceleration_x': forms.NumberInput(
+                attrs={'id': 'post-acceleration-X', 'required': True, 'placeholder': 'Enter acceleration in X direction'}
+            ),
+
+            'acceleration_y': forms.NumberInput(
+                attrs={'id': 'post-acceleration-Y', 'required': True, 'placeholder': 'Enter acceleration in Y direction'}
+            ),
+
+            'acceleration_z': forms.NumberInput(
+                attrs={'id': 'post-acceleration-Z', 'required': True, 'placeholder': 'Enter acceleration in Z direction'}
+            ),
+
+            'wheel_speed_fr': forms.NumberInput(
+                attrs={'id': 'post-wheel-speed-fr', 'required': True, 'placeholder': 'Enter speed of Front right wheel'}
+            ),
+
+            'wheel_speed_fl': forms.NumberInput(
+                attrs={'id': 'post-wheel-speed-fl', 'required': True, 'placeholder': 'Enter speed of Front left wheel'}
+            ),
+
+            'wheel_speed_br': forms.NumberInput(
+                attrs={'id': 'post-wheel-speed-br', 'required': True, 'placeholder': 'Enter speed of Rear right wheel'}
+            ),
+
+            'wheel_speed_bl': forms.NumberInput(
+                attrs={'id': 'post-wheel-speed-bl', 'required': True, 'placeholder': 'Enter speed of Rear left wheel'}
+            ),
+
+            'suspension_fr': forms.NumberInput(
+                attrs={'id': 'post-suspension-fr', 'required': True, 'placeholder': 'Enter Suspension Pressure of front right wheel'}
+            ),
+
+            'suspension_fl': forms.NumberInput(
+                attrs={'id': 'post-suspension-fl', 'required': True, 'placeholder': 'Enter Suspension Pressure of front left wheel'}
+            ),
+
+            'suspension_br': forms.NumberInput(
+                attrs={'id': 'post-suspension-br', 'required': True, 'placeholder': 'Enter Suspension Pressure of Rear right wheel'}
+            ),
+
+            'suspension_bl': forms.NumberInput(
+                attrs={'id': 'post-suspension-bl', 'required': True, 'placeholder': 'Enter Suspension Pressure of Rear left wheel'}
+            ),
+
+            'initial_fuel': forms.NumberInput(
+                attrs={'id': 'post-initial-fuel', 'required': True, 'placeholder': 'Enter initial Fuel level'}
+            ),
+
+            'fuel_decrease_rate': forms.NumberInput(
+                attrs={'id': 'post-fuel-decrease-rate', 'required': True, 'placeholder': 'Enter rate of decrease of fuel level'}
+            ),
+
+            'initial_oil': forms.NumberInput(
+                attrs={'id': 'post-initial-oil', 'required': True, 'placeholder': 'Enter initial oil level'}
+            ),
+
+            'oil_decrease_rate': forms.NumberInput(
+                attrs={'id': 'post-oil-decrease-rate', 'required': True, 'placeholder': 'Enter rate of decrease of fuel level'}
+            ),
+
+            'created_at': forms.DateTimeInput(
+                attrs={'id': 'post-created-at', 'required': True}
+            )
+        }
+# User story: #95 Continuous Submission for simulator UI
+# end
 
 #
 # class VehicleForm(ModelForm):
---
+++ mercury/static/mercury/js/main.js
@@ -0,0 +1,160 @@
+/* Created by: Rajeev
+   User story: #95 Continuous Submission for simulator UI
+   begin
+*/
+$(function() {
+
+    var buttonpressed;
+    var interval_var
+    // Submit post on submit
+    $('.submitbutton').click(function() {
+        buttonpressed = $(this).attr('name')
+    });
+    $('#SimulatorForm').on('submit', function(event){
+        event.preventDefault();
+        if(buttonpressed == "Continuous"){
+            create_post();
+            interval_var = setInterval(create_post, 10000);
+        }
+        else if(buttonpressed == "Once"){
+            if(interval_var){
+                clearInterval(interval_var);
+            }
+            create_post();
+        }
+        else if(buttonpressed == "Stop"){
+            if(interval_var){
+                clearInterval(interval_var);
+            }
+        }
+    });
+
+    // AJAX for posting
+    function create_post() {
+    console.log("create post is working!"); // sanity check
+    $.ajax({
+        url : "", // the endpoint
+        type : "POST", // http method
+        data : { the_name : $('#post-name').val(),
+        the_owner : $('#post-owner').val(),
+        the_temperature : $('#post-temperature').val(),
+        the_acceleration_x : $('#post-acceleration-X').val(),
+        the_acceleration_y : $('#post-acceleration-Y').val(),
+        the_acceleration_z : $('#post-acceleration-Z').val(),
+        the_wheel_speed_fr : $('#post-wheel-speed-fr').val(),
+        the_wheel_speed_fl : $('#post-wheel-speed-fl').val(),
+        the_wheel_speed_br : $('#post-wheel-speed-br').val(),
+        the_wheel_speed_bl : $('#post-wheel-speed-bl').val(),
+        the_suspension_fr : $('#post-suspension-fr').val(),
+        the_suspension_fl : $('#post-suspension-fl').val(),
+        the_suspension_br : $('#post-suspension-br').val(),
+        the_suspension_bl : $('#post-suspension-bl').val(),
+        the_initial_fuel : $('#post-initial-fuel').val(),
+        the_fuel_decrease_rate : $('#post-fuel-decrease-rate').val(),
+        the_initial_oil : $('#post-initial-oil').val(),
+        the_oil_decrease_rate : $('#post-oil-decrease-rate').val(),
+        the_created_at : $('#post-created-at').val()
+                 }, // data sent with the post request
+
+        // handle a successful response
+        success : function(json) {
+            generateValues()
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+
+};
+    
+    function generateValues(){
+        var random_int = Math.ceil(Math.random()*4)
+        var temperature = parseInt($('#post-temperature').val());
+        var acceleration_x = parseInt($('#post-acceleration-X').val());
+        var acceleration_y = parseInt($('#post-acceleration-Y').val());
+        var acceleration_z = parseInt($('#post-acceleration-Z').val());
+        var wheel_speed_fr = parseInt($('#post-wheel-speed-fr').val());
+        var wheel_speed_fl = parseInt($('#post-wheel-speed-fl').val());
+        var wheel_speed_br = parseInt($('#post-wheel-speed-br').val());
+        var wheel_speed_bl = parseInt($('#post-wheel-speed-bl').val());
+        var suspension_fr = parseInt($('#post-suspension-fr').val());
+        var suspension_fl = parseInt($('#post-suspension-fl').val());
+        var suspension_br = parseInt($('#post-suspension-br').val());
+        var suspension_bl = parseInt($('#post-suspension-bl').val());
+        $('#post-temperature').val(temperature+random_int);
+        $('#post-acceleration-X').val(acceleration_x+random_int);
+        $('#post-acceleration-Y').val(acceleration_y+random_int);
+        $('#post-acceleration-Z').val(acceleration_z+random_int);
+        $('#post-wheel-speed-fr').val(wheel_speed_fr+random_int);
+        $('#post-wheel-speed-fl').val(wheel_speed_fl+random_int);
+        $('#post-wheel-speed-br').val(wheel_speed_br+random_int);
+        $('#post-wheel-speed-bl').val(wheel_speed_bl+random_int);
+        $('#post-suspension-fr').val(suspension_fr+random_int);
+        $('#post-suspension-fl').val(suspension_fl+random_int);
+        $('#post-suspension-br').val(suspension_br+random_int);
+        $('#post-suspension-bl').val(suspension_bl+random_int);
+
+    }
+
+    // This function gets cookie with a given name
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    /*
+    The functions below will create a header with csrftoken
+    */
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    function sameOrigin(url) {
+        // test that a given url is a same-origin URL
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+        (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+        }
+
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                // Send the token to same-origin, relative URLs only.
+                // Send the token only if the method warrants CSRF protection
+                // Using the CSRFToken value acquired earlier
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+    });
+
+/* User story: #95 Continuous Submission for simulator UI
+   End
+*/
