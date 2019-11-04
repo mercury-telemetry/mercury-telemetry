1bfa14f34eff9fd4598a35ae6893086436b83743
de846 <de846@nyu.edu>
2019-11-03 19:41:35 -0500

    Make black happy

--- mercury/resources.py
+++ mercury/resources.py
@@ -6,6 +6,7 @@ from mercury.models import SimulatedData
 class SimulatedDataResource(ModelResource):
     """This class exposes an API for the SimulatedData object with the resource name
     defined below as the 'resource_name'."""
+
     class Meta:
         queryset = SimulatedData.objects.all()
         resource_name = "simdata"
