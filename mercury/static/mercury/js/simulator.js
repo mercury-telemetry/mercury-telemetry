318322dc2efc75e9bb416f792757bfeba48bf3e4
rajeev <rajeev.ilavala@gmail.com>
2019-11-04 21:52:18 -0500

    Removed if conditions and used slicing

--- mercury/static/mercury/js/simulator.js
+++ mercury/static/mercury/js/simulator.js
@@ -25,31 +25,6 @@ $(function () {
             }
         }
     });
-
-    function getDateTimeNow() {
-        /* This function produces a datetime timestamp for _now_ in the format of
-        "YYYY-MM-DD HH:MM:SS" */
-        let date = new Date();
-        const yyyy = date.getFullYear();
-        let dd = date.getDate();
-        let mm = (date.getMonth() + 1);
-        if (dd < 10)
-            dd = "0" + dd;
-        if (mm < 10)
-            mm = "0" + mm;
-        const cur_date = yyyy + "-" + mm + "-" + dd;
-        let hours = date.getHours()
-        let minutes = date.getMinutes()
-        let seconds = date.getSeconds();
-        if (hours < 10)
-            hours = "0" + hours;
-        if (minutes < 10)
-            minutes = "0" + minutes;
-        if (seconds < 10)
-            seconds = "0" + seconds;
-        const cur_time = hours + ":" + minutes + ":" + seconds;
-        return cur_date + " " + cur_time;
-    }
     
     // AJAX for posting
     function create_post() {
