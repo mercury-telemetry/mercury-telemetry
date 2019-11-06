2b1c32801b78dfdc4f74dd703cf8dcee1f1cbe8a
David Ecker <de846@nyu.edu>
2019-11-05 23:35:25 -0500

    Change wsgi so we can test that project name exists
    This will help inform if we change the name of `mysite` again.
    Don't run code coverage on the Travis DB line (it is implementation
    detail).

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {}
 DATABASES["default"] = dj_database_url.config(conn_max_age=600)
-if "TRAVIS" in os.environ:
+if "TRAVIS" in os.environ:  # pragma: no cover
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql_psycopg2",
