19b7a004c6139b74b8c7cc204914fca42989e356
muneebafzal <mma525@nyu.edu>
2019-11-13 22:50:13 -0500

    Adding new models for each of the sensor in models.py and register them in admin.py

--- mercury/admin.py
+++ mercury/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin  # noqa f401
 
 # Register your models here.
-from .models import SimulatedData
+from .models import SimulatedData, TemperatureSensor, AccelerationSensor, WheelSpeedSensor, SuspensionSensor,FuelLevelSensor
 
 admin.site.register(SimulatedData)
+
+admin.site.register(TemperatureSensor)
+admin.site.register(AccelerationSensor)
+admin.site.register(WheelSpeedSensor)
+admin.site.register(SuspensionSensor)
+admin.site.register(FuelLevelSensor)
+
+
--- mercury/models.py
+++ mercury/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SimulatedData(models.Model):
     """This model represents the sensors that we expect to
-    potentially available in the future in the NYU Motorsports
+    be potentially available in the future in the NYU Motorsports
     Racing vehicle."""
 
     created_at = models.DateTimeField()
@@ -36,3 +36,65 @@ class SimulatedData(models.Model):
 
     def temp(self):
         return self.temperature
+
+
+class TemperatureSensor(models.Model):
+    """This model represents the Tempeature sensor that we expect to
+    be potentially available in the future in the NYU Motorsports
+    Racing vehicle."""
+
+    created_at = models.DateTimeField()
+    # Oil temperature panel, measured in fahrenheit
+    temperature = models.FloatField(default=0)
+
+
+class AccelerationSensor(models.Model):
+    """This model represents the Acceleration sensors that we expect to
+    be potentially available in the future in the NYU Motorsports
+    Racing vehicle."""
+
+    created_at = models.DateTimeField()
+    # Acceleration Panel, measured in meter/second
+    acceleration_x = models.FloatField(default=0)
+    acceleration_y = models.FloatField(default=0)
+    acceleration_z = models.FloatField(default=0)
+
+
+class WheelSpeedSensor(models.Model):
+    """This model represents the Wheel Speed sensors that we expect to
+    be potentially available in the future in the NYU Motorsports
+    Racing vehicle."""
+
+    created_at = models.DateTimeField()
+    # Wheel Speed Panel for each of the four wheels
+    # measured in meter/second
+    wheel_speed_fr = models.FloatField(default=0)
+    wheel_speed_fl = models.FloatField(default=0)
+    wheel_speed_br = models.FloatField(default=0)
+    wheel_speed_bl = models.FloatField(default=0)
+
+
+class SuspensionSensor(models.Model):
+    """This model represents the Suspension sensors that we expect to
+    be potentially available in the future in the NYU Motorsports
+    Racing vehicle."""
+
+    created_at = models.DateTimeField()
+    # Suspension/Compression Panel for each of the four wheels
+    # measured in centimeter
+    suspension_fr = models.FloatField(default=0)
+    suspension_fl = models.FloatField(default=0)
+    suspension_br = models.FloatField(default=0)
+    suspension_bl = models.FloatField(default=0)
+
+
+class FuelLevelSensor(models.Model):
+    """This model represents the Fuel Level sensor that we expect to
+    be potentially available in the future in the NYU Motorsports
+    Racing vehicle."""
+
+    created_at = models.DateTimeField()
+    # Fuel Supply Panel
+    # measured in liters
+    current_fuel_level = models.FloatField(default=0)
+
