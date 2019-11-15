bc2d8076b9b51f950cf65cf93b6eaf5793019a4b
muneebafzal <mma525@nyu.edu>
2019-11-15 00:17:00 -0500

    Added tests for models in test_models.py, edited tests for views

--- mercury/tests/test_views.py
+++ mercury/tests/test_views.py
@@ -1,22 +1,33 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from mercury.models import SimulatedData
 from datetime import datetime
+from mercury.models import SimulatedData
 
 
 class TestViews(TestCase):
-    def test_dashboard(self):
-        client = Client()
-        response = client.get(reverse("mercury:dashboard"))
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        self.client = Client()
+        self.index_url = "mercury:index"
+        self.dashboard_url = "mercury:dashboard"
+        self.simulator_url = "mercury:simulator"
 
-    def test_simulator(self):
-        client = Client()
-        response = client.get(reverse("mercury:simulator"))
+    def test_HomePageView_GET(self):
+        response = self.client.get(reverse(self.index_url))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "index.html")
 
-    def test_simulator_post(self):
-        client = Client()
+    def test_DashboardView_GET(self):
+        response = self.client.get(reverse(self.dashboard_url))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard.html")
+
+    def test_SimulatorView_GET(self):
+        response = self.client.get(reverse(self.simulator_url))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "simulator.html")
+
+    # TODO after deciding, how to post data (no views function for the sensors as of now)
+    def test_SimulatorView_POST(self):
         test_data = {
             "temperature": 123,
             "created_at": datetime.now(),
@@ -33,11 +44,6 @@ class TestViews(TestCase):
             "suspension_bl": 0,
             "current_fuel_level": 0,
         }
-        response = client.post(reverse("mercury:simulator"), data=test_data)
+        response = self.client.post(reverse(self.simulator_url), data=test_data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(SimulatedData.objects.last().temperature, 123)
-
-    def test_index(self):
-        client = Client()
-        response = client.get(reverse("mercury:index"))
-        self.assertEqual(response.status_code, 200)
