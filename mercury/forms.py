2f672708b5cd977af99f2719a5b702b198858a0b
muneebafzal <mma525@nyu.edu>
2019-11-20 04:48:15 -0500

    cleaning the code, removing code related to SimulatedData

--- mercury/forms.py
+++ mercury/forms.py
@@ -9,55 +9,55 @@ from mercury.models import (
 )
 
 
-class SimulatorForm(forms.ModelForm):
-    class Meta:
-        model = SimulatedData
-        fields = "__all__"
-        widgets = {
-            "temperature": forms.NumberInput(
-                attrs={"id": "post-temperature", "required": True}
-            ),
-            "acceleration_x": forms.NumberInput(
-                attrs={"id": "post-acceleration-X", "required": True}
-            ),
-            "acceleration_y": forms.NumberInput(
-                attrs={"id": "post-acceleration-Y", "required": True}
-            ),
-            "acceleration_z": forms.NumberInput(
-                attrs={"id": "post-acceleration-Z", "required": True}
-            ),
-            "wheel_speed_fr": forms.NumberInput(
-                attrs={"id": "post-wheel-speed-fr", "required": True}
-            ),
-            "wheel_speed_fl": forms.NumberInput(
-                attrs={"id": "post-wheel-speed-fl", "required": True}
-            ),
-            "wheel_speed_br": forms.NumberInput(
-                attrs={"id": "post-wheel-speed-br", "required": True}
-            ),
-            "wheel_speed_bl": forms.NumberInput(
-                attrs={"id": "post-wheel-speed-bl", "required": True}
-            ),
-            "suspension_fr": forms.NumberInput(
-                attrs={"id": "post-suspension-fr", "required": True}
-            ),
-            "suspension_fl": forms.NumberInput(
-                attrs={"id": "post-suspension-fl", "required": True}
-            ),
-            "suspension_br": forms.NumberInput(
-                attrs={"id": "post-suspension-br", "required": True}
-            ),
-            "suspension_bl": forms.NumberInput(
-                attrs={"id": "post-suspension-bl", "required": True}
-            ),
-            "current_fuel_level": forms.NumberInput(
-                attrs={"id": "post-current-fuel-level", "required": True}
-            ),
-            "created_at": forms.DateTimeInput(
-                attrs={"id": "post-created-at", "required": True}
-            ),
-        }
-
+# class SimulatorForm(forms.ModelForm):
+#     class Meta:
+#         model = SimulatedData
+#         fields = "__all__"
+#         widgets = {
+#             "temperature": forms.NumberInput(
+#                 attrs={"id": "post-temperature", "required": True}
+#             ),
+#             "acceleration_x": forms.NumberInput(
+#                 attrs={"id": "post-acceleration-X", "required": True}
+#             ),
+#             "acceleration_y": forms.NumberInput(
+#                 attrs={"id": "post-acceleration-Y", "required": True}
+#             ),
+#             "acceleration_z": forms.NumberInput(
+#                 attrs={"id": "post-acceleration-Z", "required": True}
+#             ),
+#             "wheel_speed_fr": forms.NumberInput(
+#                 attrs={"id": "post-wheel-speed-fr", "required": True}
+#             ),
+#             "wheel_speed_fl": forms.NumberInput(
+#                 attrs={"id": "post-wheel-speed-fl", "required": True}
+#             ),
+#             "wheel_speed_br": forms.NumberInput(
+#                 attrs={"id": "post-wheel-speed-br", "required": True}
+#             ),
+#             "wheel_speed_bl": forms.NumberInput(
+#                 attrs={"id": "post-wheel-speed-bl", "required": True}
+#             ),
+#             "suspension_fr": forms.NumberInput(
+#                 attrs={"id": "post-suspension-fr", "required": True}
+#             ),
+#             "suspension_fl": forms.NumberInput(
+#                 attrs={"id": "post-suspension-fl", "required": True}
+#             ),
+#             "suspension_br": forms.NumberInput(
+#                 attrs={"id": "post-suspension-br", "required": True}
+#             ),
+#             "suspension_bl": forms.NumberInput(
+#                 attrs={"id": "post-suspension-bl", "required": True}
+#             ),
+#             "current_fuel_level": forms.NumberInput(
+#                 attrs={"id": "post-current-fuel-level", "required": True}
+#             ),
+#             "created_at": forms.DateTimeInput(
+#                 attrs={"id": "post-created-at", "required": True}
+#             ),
+#         }
+#
 
 class TemperatureForm(forms.ModelForm):
     class Meta:
--- mercury/views/simulator.py
+++ mercury/views/simulator.py
@@ -28,38 +28,6 @@ class SimulatorView(TemplateView):
     def post(self, request, *args, **kwargs):
         """Used by AJAX method in the simulator.js file to save data
         from the simulator UI."""
-        # if request.POST.get("created_at"):
-        #     post_created_at = request.POST.get("created_at")
-        #     post_temperature = request.POST.get("temperature")
-        #     post_acceleration_x = request.POST.get("acceleration_x")
-        #     post_acceleration_y = request.POST.get("acceleration_y")
-        #     post_acceleration_z = request.POST.get("acceleration_z")
-        #     post_wheel_speed_fr = request.POST.get("wheel_speed_fr")
-        #     post_wheel_speed_fl = request.POST.get("wheel_speed_fl")
-        #     post_wheel_speed_br = request.POST.get("wheel_speed_br")
-        #     post_wheel_speed_bl = request.POST.get("wheel_speed_bl")
-        #     post_suspension_fr = request.POST.get("suspension_fr")
-        #     post_suspension_fl = request.POST.get("suspension_fl")
-        #     post_suspension_br = request.POST.get("suspension_br")
-        #     post_suspension_bl = request.POST.get("suspension_bl")
-        #     post_current_fuel_level = request.POST.get("current_fuel_level")
-        #     sim_data = SimulatedData(
-        #         created_at=post_created_at,
-        #         temperature=post_temperature,
-        #         acceleration_x=post_acceleration_x,
-        #         acceleration_y=post_acceleration_y,
-        #         acceleration_z=post_acceleration_z,
-        #         wheel_speed_fr=post_wheel_speed_fr,
-        #         wheel_speed_fl=post_wheel_speed_fl,
-        #         wheel_speed_br=post_wheel_speed_br,
-        #         wheel_speed_bl=post_wheel_speed_bl,
-        #         suspension_fr=post_suspension_fr,
-        #         suspension_fl=post_suspension_fl,
-        #         suspension_br=post_suspension_br,
-        #         suspension_bl=post_suspension_bl,
-        #         current_fuel_level=post_current_fuel_level,
-        #     )
-        #     sim_data.save()
 
         if request.POST.get("created_at_temp"):
             post_created_at = request.POST.get("created_at_temp")
