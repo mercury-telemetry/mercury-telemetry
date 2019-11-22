18b521bf57087f421f94800b349f3f2ab2e43d39
David Ecker <de846@nyu.edu>
2019-11-22 10:18:30 -0500

    Try to fix travis build

--- mercury/apps.py
+++ mercury/apps.py
@@ -1,11 +1,17 @@
 from django.apps import AppConfig
-from django.db.utils import OperationalError
 
 
 class MercuryConfig(AppConfig):
     name = "mercury"
 
     def ready(self):
+        """This block is a Django hook that runs when the app is ready. It is
+        designed to be used during the "testing sprint" since there was a
+        requirement that the acccount (event code in our case) could
+        survive a system restart and database reset. This entire method
+        should be removed when it is no longer needed. The bare except block
+        protects the app from failing when running manage.py commands to
+        make and use DB migration files."""
         try:
             from .models import EventCodeAccess
 
@@ -14,5 +20,5 @@ class MercuryConfig(AppConfig):
                     code.delete()
             test_code = EventCodeAccess(event_code="testcode", enabled=True)
             test_code.save()
-        except OperationalError:
+        except:  # noqa E722
             pass
