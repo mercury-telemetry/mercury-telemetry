ea9c5e1bbb6fd96e6b5633747252b378ae88b34f
David Ecker <de846@nyu.edu>
2019-11-22 09:24:23 -0500

    Fix broken travis build

--- mercury/apps.py
+++ mercury/apps.py
@@ -1,14 +1,18 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError
 
 
 class MercuryConfig(AppConfig):
     name = "mercury"
 
     def ready(self):
-        from .models import EventCodeAccess
+        try:
+            from .models import EventCodeAccess
 
-        for code in EventCodeAccess.objects.all():
-            if code.event_code == "testcode":
-                code.delete()
-        test_code = EventCodeAccess(event_code="testcode", enabled=True)
-        test_code.save()
+            for code in EventCodeAccess.objects.all():
+                if code.event_code == "testcode":
+                    code.delete()
+            test_code = EventCodeAccess(event_code="testcode", enabled=True)
+            test_code.save()
+        except OperationalError:
+            pass
