15c6bc7700990975b72f5fc7c065919f2fb63cec
David Ecker <de846@nyu.edu>
2019-11-22 09:08:20 -0500

    Add always active testcode event_code

--- mercury/apps.py
+++ mercury/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class MercuryConfig(AppConfig):
     name = "mercury"
+
+    def ready(self):
+        from .models import EventCodeAccess
+        for code in EventCodeAccess.objects.all():
+            if code.event_code == "testcode":
+                code.delete()
+        test_code = EventCodeAccess(event_code="testcode", enabled=True)
+        test_code.save()
