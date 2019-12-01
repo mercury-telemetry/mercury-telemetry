f9a715d916e64f5a058046fc6dcf325311c4cf27
David Ecker <de846@nyu.edu>
2019-12-01 16:01:05 -0500

    Print error response on CAN Decoder UI

--- mercury/static/mercury/js/can.js
+++ mercury/static/mercury/js/can.js
@@ -27,10 +27,13 @@ $(function () {
             },
 
             // handle a non-successful response
-            error: function (xhr, errmsg, err) {
+            error: function (xhr, errmsg) {
                 $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " + errmsg +
                     " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
                 console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+                let json_resp = JSON.stringify(xhr.responseText, undefined, 2).replace(/\\\"/g, "").replace(/,/g, ",\n");
+                document.getElementById("result").innerHTML = json_resp;
+
             }
         });
     }
