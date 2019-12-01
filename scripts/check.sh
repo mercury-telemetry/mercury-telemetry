31ccc1f155cd4c411160268c71874cf665b671e2
David Ecker <de846@nyu.edu>
2019-12-01 15:54:04 -0500

    Ignore flake8 line-too-long errors in tests only

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,5 +2,6 @@
 set -e
 cd "$(git rev-parse --show-toplevel)"
 black --check mysite mercury
-flake8 mysite mercury
+flake8 --exclude tests mysite/ mercury/
+flake8 --ignore=E501 mercury/tests/
 python3 manage.py test
