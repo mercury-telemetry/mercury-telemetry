5a7c79fdfda06eb917fb7db9f16b41cfbfa82a07
Ziheng-Wang <bwg9623@gmail.com>
2020-03-02 18:23:47 -0500

    create new database model

--- mercury/models.py
+++ mercury/models.py
@@ -99,3 +99,63 @@ class EventCodeAccess(models.Model):
 
     def __str__(self):  # pragma: no cover
         return EventCodeAccess.__name__
+
+
+class Events(models.Model):
+    """This model stores the information about events. When a new event is created,
+    an auto-incremented event_id will be assigned to this event and also store the current
+    date and location for this event. """
+    event_id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=40, unique=True, null=False, blank=False)
+    event_date = models.DateTimeField()
+    event_loc_lat = models.FloatField(default=0, blank=True)
+    event_loc_lon = models.FloatField(default=0, blank=True)
+    event_description = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return Events.__name__
+
+
+class Sensor(models.Model) :
+    """This model stores the information about sensor. When a new sensor is created,
+        an auto-incremented sensor_id will be assigned to this event and also store the name
+        and description for this sensor . """
+    sensor_id = models.AutoField(primary_key=True)
+    sensor_name = models.CharField(max_length=40, unique=True, null=False, blank=False)
+    sensor_description = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return Sensor.__name__
+
+
+class Field(models.Model) :
+    """This model stores the information about fields of sensors. """
+    field_id = models.AutoField(primary_key=True)
+    sensor_id = models.ForeignKey(Sensor, related_name='sensor_field',
+                                  to_field="sensor_id",
+                                  primary_key=True,
+                                  on_delete=models.CASCADE)
+    field_name = models.CharField(max_length=40, null=False, blank=False)
+
+    def __str__(self):
+        return Field.__name__
+
+
+class General_data(models.Model) :
+    event_id = models.ForeignKey(Events, related_name="event_data",
+                                 on_delete=models.CASCADE,
+                                 to_field='event_id',
+                                 primary_key=True)
+    sensor_id = models.ForeignKey(Sensor, related_name="sensor_data",
+                                  on_delete=models.CASCADE,
+                                  to_field='sensor_id',
+                                  primary_key=True)
+    field_id = models.ForeignKey(Field, related_name="field_data",
+                                 on_delete=models.CASCADE,
+                                 to_field='field_id',
+                                 primary_key=True)
+    stored_at_time = models.DateTimeField()     # index
+    data_value = models.FloatField(default=0)
+
+    def __str__(self):
+        return General_data.__name__
