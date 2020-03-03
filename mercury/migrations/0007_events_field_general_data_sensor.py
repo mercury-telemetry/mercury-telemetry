73846e55dedd9a8914da4ecf4a5aeaa13bbc72dc
Ziheng-Wang <bwg9623@gmail.com>
2020-03-02 21:28:36 -0500

    creating a new database model

--- mercury/migrations/0007_events_field_general_data_sensor.py
+++ mercury/migrations/0007_events_field_general_data_sensor.py
@@ -5,55 +5,101 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('mercury', '0006_delete_simulateddata'),
+        ("mercury", "0006_delete_simulateddata"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Events',
+            name="Events",
             fields=[
-                ('event_id', models.AutoField(primary_key=True, serialize=False)),
-                ('event_name', models.CharField(max_length=40, unique=True)),
-                ('event_date', models.DateTimeField()),
-                ('event_loc_lat', models.FloatField(blank=True, default=0)),
-                ('event_loc_lon', models.FloatField(blank=True, default=0)),
-                ('event_description', models.CharField(blank=True, max_length=100, null=True)),
+                ("event_id", models.AutoField(primary_key=True, serialize=False)),
+                ("event_name", models.CharField(max_length=40, unique=True)),
+                ("event_date", models.DateTimeField()),
+                ("event_loc_lat", models.FloatField(blank=True, default=0)),
+                ("event_loc_lon", models.FloatField(blank=True, default=0)),
+                (
+                    "event_description",
+                    models.CharField(blank=True, max_length=100, null=True),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Sensor',
+            name="Sensor",
             fields=[
-                ('sensor_id', models.AutoField(primary_key=True, serialize=False)),
-                ('sensor_name', models.CharField(max_length=40, unique=True)),
-                ('sensor_description', models.CharField(blank=True, max_length=100, null=True)),
+                ("sensor_id", models.AutoField(primary_key=True, serialize=False)),
+                ("sensor_name", models.CharField(max_length=40, unique=True)),
+                (
+                    "sensor_description",
+                    models.CharField(blank=True, max_length=100, null=True),
+                ),
             ],
         ),
         migrations.CreateModel(
-            name='Field',
+            name="Field",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('field_id', models.IntegerField(unique=True)),
-                ('field_name', models.CharField(max_length=40)),
-                ('sensor_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='sensor_field', to='mercury.Sensor')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("field_id", models.IntegerField(unique=True)),
+                ("field_name", models.CharField(max_length=40)),
+                (
+                    "sensor_id",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="sensor_field",
+                        to="mercury.Sensor",
+                    ),
+                ),
             ],
-            options={
-                'unique_together': {('field_id', 'sensor_id')},
-            },
+            options={"unique_together": {("field_id", "sensor_id")}, },
         ),
         migrations.CreateModel(
-            name='General_data',
+            name="General_data",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('stored_at_time', models.DateTimeField()),
-                ('data_value', models.FloatField(default=0)),
-                ('event_id', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='event_data', to='mercury.Events')),
-                ('field_id', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='field_data', to='mercury.Field', to_field='field_id')),
-                ('sensor_id', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='sensor_data', to='mercury.Sensor')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("stored_at_time", models.DateTimeField()),
+                ("data_value", models.FloatField(default=0)),
+                (
+                    "event_id",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="event_data",
+                        to="mercury.Events",
+                    ),
+                ),
+                (
+                    "field_id",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="field_data",
+                        to="mercury.Field",
+                        to_field="field_id",
+                    ),
+                ),
+                (
+                    "sensor_id",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="sensor_data",
+                        to="mercury.Sensor",
+                    ),
+                ),
             ],
-            options={
-                'unique_together': {('event_id', 'sensor_id', 'field_id')},
-            },
+            options={"unique_together": {("event_id", "sensor_id", "field_id")}, },
         ),
     ]
--- mercury/models.py
+++ mercury/models.py
@@ -105,6 +105,7 @@ class Events(models.Model):
     """This model stores the information about events. When a new event is created,
     an auto-incremented event_id will be assigned to this event and also store the current
     date and location for this event. """
+
     event_id = models.AutoField(primary_key=True)
     event_name = models.CharField(max_length=40, unique=True, null=False, blank=False)
     event_date = models.DateTimeField()
@@ -116,10 +117,11 @@ class Events(models.Model):
         return Events.__name__
 
 
-class Sensor(models.Model) :
+class Sensor(models.Model):
     """This model stores the information about sensor. When a new sensor is created,
         an auto-incremented sensor_id will be assigned to this event and also store the name
         and description for this sensor . """
+
     sensor_id = models.AutoField(primary_key=True)
     sensor_name = models.CharField(max_length=40, unique=True, null=False, blank=False)
     sensor_description = models.CharField(max_length=100, null=True, blank=True)
@@ -128,12 +130,16 @@ class Sensor(models.Model) :
         return Sensor.__name__
 
 
-class Field(models.Model) :
+class Field(models.Model):
     """This model stores the information about fields of sensors. """
+
     field_id = models.IntegerField(unique=True)
-    sensor_id = models.ForeignKey(Sensor, related_name='sensor_field',
-                                  to_field="sensor_id",
-                                  on_delete=models.CASCADE)
+    sensor_id = models.ForeignKey(
+        Sensor,
+        related_name="sensor_field",
+        to_field="sensor_id",
+        on_delete=models.CASCADE,
+    )
     field_name = models.CharField(max_length=40, null=False, blank=False)
 
     class Meta:
@@ -143,20 +149,23 @@ class Field(models.Model) :
         return Field.__name__
 
 
-class General_data(models.Model) :
-    event_id = models.OneToOneField(Events, related_name="event_data",
-                                 on_delete=models.CASCADE,
-                                 to_field='event_id')
-    sensor_id = models.OneToOneField(Sensor, related_name="sensor_data",
-                                  on_delete=models.CASCADE,
-                                  to_field='sensor_id')
-    field_id = models.OneToOneField(Field, related_name="field_data",
-                                 on_delete=models.CASCADE,
-                                 to_field='field_id')
-    stored_at_time = models.DateTimeField()     # index
+class General_data(models.Model):
+    event_id = models.OneToOneField(
+        Events, related_name="event_data", on_delete=models.CASCADE, to_field="event_id"
+    )
+    sensor_id = models.OneToOneField(
+        Sensor,
+        related_name="sensor_data",
+        on_delete=models.CASCADE,
+        to_field="sensor_id",
+    )
+    field_id = models.OneToOneField(
+        Field, related_name="field_data", on_delete=models.CASCADE, to_field="field_id"
+    )
+    stored_at_time = models.DateTimeField()  # index
     data_value = models.FloatField(default=0)
 
-    class Meta :
+    class Meta:
         unique_together = (("event_id", "sensor_id", "field_id"),)
 
     def __str__(self):
