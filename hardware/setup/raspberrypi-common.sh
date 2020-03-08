ba9edbe13d3a1d1d681ba5d4f55a6170a63a9cdc
PrabhanshuAttri <contact@prabhanshu.com>
2020-03-08 01:00:48 -0500

    added display resolution for remote access

--- hardware/setup/raspberrypi-common.sh
+++ hardware/setup/raspberrypi-common.sh
@@ -4,6 +4,8 @@ function showStatus() {
 
 showStatus 'Running Raspberry Pi Common Setup'
 
+cd ~/Downloads
+
 showStatus 'Updating system...'
 sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
@@ -13,6 +15,9 @@ sudo raspi-config nonint do_vnc 0
 showStatus 'Enabling SSH'
 sudo raspi-config nonint do_ssh 0
 
+showStatus 'Setting screen resolution for remote access'
+sudo raspi-config nonint do_resolution 2 16
+
 showStatus 'Installing AnyDesk'
 wget https://download.anydesk.com/rpi/anydesk_5.5.4-1_armhf.deb
 sudo dpkg -i anydesk*.deb
@@ -20,3 +25,4 @@ sudo apt-get install -f
 sudo systemctl daemon-reload
 
 showStatus 'Completed Raspberry Pi Common Setup'
+cd ~/
