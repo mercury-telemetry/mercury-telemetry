22e30e968dbb393adbf13715e9d866cd33a7dc8c
PrabhanshuAttri <contact@prabhanshu.com>
2020-03-14 21:46:11 -0400

    clean up

--- hardware/CommunicationsPi/utils.py
+++ hardware/CommunicationsPi/utils.py
@@ -2,12 +2,9 @@ import os
 import json
 from logger import Logger
 
-
 def get_serial_stream(s):
     return (json.dumps(s) + "\n").encode()
 
-
 def get_logger(key):
-    print(key, os.environ[key])
     logger = Logger(name=key, filename=os.environ[key])
     return logger
