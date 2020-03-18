364bc7cd6f52ccd2b0a5eab9e75df385d10eea73
ray310 <60367095+Ray310@users.noreply.github.com>
2020-03-18 19:52:35 -0400

    1) Updated views/sensor.py to refelct new database schema 2) Modified appearance and functioning of sensor.html page and associated javascript (sensor.js) 	2a) Included deleted row from add sensor view

--- mercury/static/mercury/js/sensor.js
+++ mercury/static/mercury/js/sensor.js
@@ -1,51 +1,22 @@
 function addRow(){
-  var sensorList, field_name_li, field_type_li, field_name_input, field_type_select,
-  float_option, string_option, bool_option;
-
-  fields = document.getElementById("field-list");
-  // Make field name input
-  field_name_input = document.createElement("input");
-  field_name_input.setAttribute("type","text");
-  field_name_input.setAttribute("name","field-name");
-  field_name_input.setAttribute("class","inline-block-child");
-  fields.appendChild(field_name_input);
-
-  //Make field type
-  field_type_select = document.createElement("select");
-  field_type_select.setAttribute("name", "field-type");
-  field_type_select.setAttribute("class", "inline-block-child");
-
-  float_option = document.createElement("option");
-  float_option.setAttribute("value", "float");
-  float_option.appendChild(document.createTextNode("Float"));
-  field_type_select.appendChild(float_option);
-
-  string_option = document.createElement("option");
-  string_option.setAttribute("value", "string");
-  string_option.appendChild(document.createTextNode("String"));
-  field_type_select.appendChild(string_option);
-
-  bool_option = document.createElement("option");
-  bool_option.setAttribute("value", "bool");
-  bool_option.appendChild(document.createTextNode("Boolean"));
-  field_type_select.appendChild(bool_option);
-
-  fields.appendChild(field_type_select);
-
-  // Make unit input
-  unit_input = document.createElement("input");
-  unit_input.setAttribute("type","text");
-  unit_input.setAttribute("name","field-unit")
-  unit_input.setAttribute("class", "inline-block-child");
-  fields.appendChild(unit_input);
-
-  var hr = document.createElement('hr');
-  fields.appendChild(hr);
+    var table = document.getElementById("new-sensor-table");
+    var rowCount = table.rows.length;
+    var colCount = table.rows[0].cells.length;
+    var newRow = table.insertRow(rowCount);
+    for(var i=0; i<colCount; i++){
+        var newCell = newRow.insertCell(i);
+        /* set new cell to be same as cell in first data (non-header) row */
+        newCell.innerHTML = table.rows[1].cells[i].innerHTML
+    }
+    table.rows[rowCount].cells[0].innerHTML =rowCount
 }
 
-var add_field_button = document.getElementById("addfieldbutton");
-add_field_button.onclick = function() {
-  addRow();
+function deleteRow() {
+    var table = document.getElementById("new-sensor-table");
+    var rowCount = table.rows.length;
+    if (rowCount > 2){
+        table.deleteRow(rowCount-1)
+    }
 }
 
 /*
@@ -54,10 +25,10 @@ selectView displays the correct view of the "Add or Modify Sensors" page
 -if a user submits a new sensor which posts and reloads page, then the add new sensor view should remain
 */
 function selectView() {
-  var newSensorDiv = document.getElementById("add-new-sensor");
-  var existingSensorDiv = document.getElementById("existing-sensors");
-  var modifyButton = document.getElementById("current-sensor-btn");
-  var newButton = document.getElementById("add-sensor-btn");
+    var newSensorDiv = document.getElementById("add-new-sensor");
+    var existingSensorDiv = document.getElementById("existing-sensors");
+    var modifyButton = document.getElementById("current-sensor-btn");
+    var newButton = document.getElementById("add-sensor-btn");
     if (sessionStorage.getItem('viewing') == 'addingNew'){
         newSensorDiv.style.display = "block";
         existingSensorDiv.style.display = 'none';
@@ -75,8 +46,8 @@ function selectView() {
 }
 
 function displayCurrentSensors() {
-  sessionStorage.setItem('viewing', 'existing');
-  selectView()
+    sessionStorage.setItem('viewing', 'existing');
+    selectView()
 }
 
 function displayAddNewSensor() {
--- mercury/views/sensor.py
+++ mercury/views/sensor.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from ..event_check import require_event_code
 from mercury.models import AGSensor
+from mercury.models import FuelLevelSensor
 from django.contrib import messages
 
 log = logging.getLogger(__name__)
@@ -30,61 +31,34 @@ class CreateSensorView(TemplateView):
 
     @require_event_code
     def post(self, request, *args, **kwargs):
-        post_sensor_name = request.POST.get("sensor-name")
-        post_field_names = request.POST.getlist("field-name")
-        post_field_types = request.POST.getlist("field-type")
-        post_field_units = request.POST.getlist("field-unit")
+        id_num = request.POST.get("sensor-id-num")
+        sensor_name = request.POST.get("sensor-name")
+        field_names = request.POST.getlist("field-names")
+        field_types = request.POST.getlist("data-types")
+        field_units = request.POST.getlist("units")
 
-        field_names = list()
-        field_types = list()
-        field_units = list()
-        for i in range(len(post_field_names)):
-            if post_field_names[i]:
-                field_names.append(post_field_names[i])
-                field_types.append(post_field_types[i])
-                field_units.append(post_field_units[i])
-
+        # error checking. Note that sensor name, field name and id number are required by the HTML form. See sensor.HTML
         form_valid = True
-
-        if not post_sensor_name:
-            messages.error(
-                request, ("Sensor name is missing."),
-            )
+        if len(AGSensor.objects.filter(sensor_name=sensor_name)) > 0:
+            messages.error(request, "Sensor name is already taken.")
             form_valid = False
-
-        sensor = AGSensor.objects.filter(sensor_name=post_sensor_name)
-        if sensor.count() > 0:
-            messages.error(
-                request, ("Sensor name already taken."),
-            )
-            form_valid = False
-
-        if len(field_names) == 0:
-            messages.error(
-                request, ("Sensor must have at least 1 field."),
-            )
-            form_valid = False
-
         if check_if_duplicates(field_names):
-            messages.error(
-                request, ("Field names must be unique."),
-            )
+            messages.error(request, "Field names must be unique.")
             form_valid = False
 
+        # create sensor format which is dictionary of dictionaries
         sensor_format = {}
-        for i in range(len(field_names)):
-            if post_field_names[i]:
-                sensor_format[field_names[i]] = {
-                    "unit": field_units[i],
-                    "format": field_types[i],
-                }
+        fields = zip(field_names, field_types, field_units)
+        for field in fields:
+            sensor_format[field[0]] = {"data_type": field[1], "unit": field[2]}
 
         if form_valid:
             sensor = AGSensor.objects.create(
-                sensor_name=post_sensor_name,
+                sensor_id=id_num,
+                sensor_name=sensor_name,
                 sensor_processing_formula=0,
                 sensor_format=sensor_format,
-            )
+            )  # processing formula needs to be adapted
             sensor.save()
             sensors = AGSensor.objects.all()
             context = {"sensors": sensors}
@@ -92,7 +66,7 @@ class CreateSensorView(TemplateView):
             sensors = AGSensor.objects.all()
             context = {
                 "sensors": sensors,
-                "sensor_name": post_sensor_name,
+                "sensor_name": sensor_name,
                 "sensor_format": sensor_format,
             }
 
