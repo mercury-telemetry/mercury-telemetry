5055604368330d1301303fb7cb68a7481395897e
PrabhanshuAttri <contact@prabhanshu.com>
2020-03-17 21:57:19 -0400

    Added lan_client

---
+++ hardware/CommunicationsPi/request_lan_server.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from requests.exceptions import HTTPError
+from utils import get_logger
+
+logging = get_logger("LAN_CLIENT_LOG_FILE")
+
+url = os.environ["LAN_SERVER"]
+
+while True:
+    try:
+        payload = {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+        loggin.info('pinging: ' + url)
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        logging.error('HTTP error occurred: {}'.format(str(http_err)))
+    except Exception as err:
+        logging.error('error occurred: {}'.format(str(err)))
+    else:
+        logging.info('Success!')
