b1c999ccd4cedfae74ac74e28edaf3c29c4e193a
Daisy Crego <daisycrego@gmail.com>
2020-03-23 15:34:43 -0400

    Fix migrations files to handle recent merge.

--- mercury/models.py
+++ mercury/models.py
@@ -1,40 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import JSONField
-import uuid
-from django.utils import timezone
-
-
-class AGEvent(models.Model):
-    """This model stores the information about events. When a new event is created,
-    a UUID4-typed event_uuid will be assigned to this event and also store the current
-    date for this event. """
-
-    event_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    event_name = models.CharField(max_length=40, blank=True)
-    event_date = models.DateTimeField(default=timezone.now)
-    event_description = models.CharField(max_length=100, null=False, blank=True)
-    event_location = models.CharField(max_length=100, null=False, blank=True)
-
-
-class AGSensor(models.Model):
-    sensor_id = models.AutoField(primary_key=True)
-    sensor_name = models.CharField(max_length=1024, blank=True)
-    sensor_processing_formula = models.IntegerField(default=0, null=False)
-    sensor_format = JSONField()
-
-
-class AGMeasurement(models.Model):
-    measurement_uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
-    measurement_timestamp = models.DateTimeField(default=timezone.now, blank=False)
-    measurement_event = models.ForeignKey(
-        AGEvent, on_delete=models.CASCADE, blank=False, null=False
-    )
-    measurement_sensor = models.ForeignKey(
-        AGSensor, on_delete=models.CASCADE, blank=False, null=False
-    )
-    measurement_value = JSONField()
 
 
 class GFConfig(models.Model):
