d9e05d6da2ea4199ad59efa92ec0bf3cb1f15a9b
PrabhanshuAttri <contact@prabhanshu.com>
2020-04-01 17:18:38 -0400

    removed old tests

--- hardware/CommunicationsPi/test_lan_server.py
+++
@@ -1,8 +0,0 @@
-import lan_client_class
-
-if __name__ == "__main__":
-
-    # log_file_name and lan_server_url are configurable for lan_client
-    client = lan_client_class.LANClient()
-    sample_payload = {"key1": "value1", "key2": "value2"}
-    client.ping_lan_server(sample_payload)
