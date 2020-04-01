08020980115a56438390cd2b565704913280a8e4
PrabhanshuAttri <contact@prabhanshu.com>
2020-04-01 15:47:35 -0400

    Added port searching

--- hardware/CommunicationsPi/radio_transceiver.py
+++ hardware/CommunicationsPi/radio_transceiver.py
@@ -2,6 +2,7 @@ import os
 import time
 import serial
 import json
+import serial.tools.list_ports
 
 from utils import get_logger, get_serial_stream
 
@@ -11,24 +12,65 @@ class Transceiver:
         if log_file_name is None:
             self.logging = get_logger("TRANSMITTER_LOG_FILE")
         else:
-            self.logging = get_logger(log_file_name)
+            self.logging = get_logger(log_file_name, log_file_name)
 
-        port = os.environ["RADIO_TRANSMITTER_PORT"] if port is None else port
+        self.port = os.environ["RADIO_TRANSMITTER_PORT"] if port is None else port
+
+        if not self.port:
+            self.port_vid = None
+            self.port_pid = None
+            self.port_vendor = None
+            self.port_intf = None
+            self.port_serial_number = None
+            self.find_port()
+
         baudrate = 9600
         parity = serial.PARITY_NONE
         stopbits = serial.STOPBITS_ONE
         bytesize = serial.EIGHTBITS
         timeout = 1
 
-        self.logging.info("Opening serial")
+        self.logging.info("Opening serial on: " + str(self.port))
         self.serial = serial.Serial(
-            port=port,
+            port=self.port,
             baudrate=baudrate,
             parity=parity,
             stopbits=stopbits,
             bytesize=bytesize,
             timeout=timeout,
         )
+
+
+    def find_port(self):
+        for port in serial.tools.list_ports.comports():
+            if self.is_usb_serial(port):
+                print(port)
+                print('D', port.device)
+                self.port = port.device
+                return
+
+        return
+
+
+    def is_usb_serial(self, port):
+        if port.vid is None:
+            return False
+        if not self.port_vid is None:
+            if port.vid != self.port_vid:
+                return False
+        if not self.port_pid is None:
+            if port.pid != self.port_pid:
+                return False
+        if not self.port_vendor is None:
+            if not port.manufacturer.startswith(self.port_vendor):
+                return False
+        if not self.port_serial_number is None:
+            if not port.serial_number.startswith(self.port_serial_number):
+                return False
+        if not self.port_intf is None:
+            if port.interface is None or not self.port_intf in port.interface:
+                return False
+        return True
 
     def send(self, payload):
         self.logging.info("sending")
