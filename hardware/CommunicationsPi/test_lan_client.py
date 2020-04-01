ed8a698333c09d56f3eb7dd0dfd599f0417d9a96
Yashasvi Dadhe <yd1405@nyu.edu>
2020-04-01 16:52:35 -0400

    Changed import module name

--- hardware/CommunicationsPi/test_lan_client.py
+++ hardware/CommunicationsPi/test_lan_client.py
@@ -1,10 +1,10 @@
 from sys import argv
-import lan_server_class
+import lan_server
 
 if __name__ == "__main__":
 
     if len(argv) == 2:
         # log_file_name and port are configurable for lan_server
-        lan_server_class.run(port=int(argv[1]))
+        lan_server.run(port=int(argv[1]))
     else:
-        lan_server_class.run()
+        lan_server.run()
--- hardware/CommunicationsPi/test_lan_server.py
+++ hardware/CommunicationsPi/test_lan_server.py
@@ -1,8 +1,8 @@
-import lan_client_class
+import lan_client
 
 if __name__ == "__main__":
 
     # log_file_name and lan_server_url are configurable for lan_client
-    client = lan_client_class.LANClient()
+    client = lan_client.LANClient()
     sample_payload = {"key1": "value1", "key2": "value2"}
     client.ping_lan_server(sample_payload)
