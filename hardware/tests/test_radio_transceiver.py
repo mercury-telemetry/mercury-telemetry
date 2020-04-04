53946df91021028eab0ef1f713ccb33767720a7d
Alex Biehl <ab7289@nyu.edu>
2020-04-04 19:21:27 -0400

    added additional init test

--- hardware/tests/test_radio_transceiver.py
+++ hardware/tests/test_radio_transceiver.py
@@ -58,3 +58,44 @@ class TranscieverTests(SimpleTestCase):
             self.assertIsNone(transciever.port_vendor)
             self.assertIsNone(transciever.port_intf)
             self.assertIsNone(transciever.port_serial_number)
+
+    @mock.patch("serial.Serial")
+    @mock.patch("serial.tools.list_ports.comports")
+    def test_init_dir(self, mock_port_list, mock_serial):
+        """
+        Tests the __init__ function where log dir is specified
+        """
+
+        mock_port_list.return_value = [
+            {
+                "vid": "vid",
+                "pid": None,
+                "manufacturer": None,
+                "serial_number": None,
+                "interface": None,
+                "device": "usb",
+            }
+        ]
+
+        # mock_serial = mock.Mock()
+
+        with mock.patch.dict(
+            os.environ,
+            {
+                "LOG_DIRECTORY": self.temp_dir.path,
+                "RADIO_TRANSMITTER_PORT": "",
+                "LOG_FILE": "logger.txt",
+            },
+        ):
+            transciever = Transceiver(log_file_name="LOG_FILE")
+
+            self.assertTrue(transciever.logging is not None)
+            self.assertTrue(transciever.logging.name == "LOG_FILE")
+            self.assertIsInstance(transciever.logging, Logger)
+
+            self.assertTrue(transciever.port == "usb")
+            self.assertIsNone(transciever.port_vid)
+            self.assertIsNone(transciever.port_pid)
+            self.assertIsNone(transciever.port_vendor)
+            self.assertIsNone(transciever.port_intf)
+            self.assertIsNone(transciever.port_serial_number)
