05f61a922f138c438e6a13b2449a1cc7c4d3f956
Michael Lally <mikelally801@gmail.com>
2020-04-08 07:33:04 +0000

    added post to placeholder endpoint

--- hardware/SensorPi/main.py
+++ hardware/SensorPi/main.py
@@ -1,23 +1,40 @@
 from sense_hat import SenseHat
 import json
+import requests
 from datetime import datetime
 from sensehat_reader import *
+
+API_ENDPOINT = "http://pastebin.com/api/api_post.php"
 
 sense = SenseHat()
 nyu_purple = (87, 46, 140)
 sense.show_message("MERCURY", text_colour=nyu_purple, scroll_speed=0.04)
 sense.clear()
 
-json_temperature  = temperature(sense)
-json_pressure     = pressure(sense)
-json_humidity     = humidity(sense)
-json_acceleration = acceleration(sense)
-json_orientation  = orientation(sense)
-
-#print(json_temperature)
-#print(json_pressure)
-#print(json_humidity)
-#print(json_acceleration)
-#print(json_orientation)
+while(1):
+    
+    #json_temperature  = temperature(sense)
+    #json_pressure     = pressure(sense)
+    #json_humidity     = humidity(sense)
+    #json_acceleration = acceleration(sense)
+    #json_orientation  = orientation(sense)
+    
+    json_temperature  = json.dumps(temperature(sense))
+    json_pressure     = json.dumps(pressure(sense))
+    json_humidity     = json.dumps(humidity(sense))
+    json_acceleration = json.dumps(acceleration(sense))
+    json_orientation  = json.dumps(orientation(sense))
+    
+    response_temperature  = requests.post(url=API_ENDPOINT, data=json_temperature)
+    response_pressure     = requests.post(url=API_ENDPOINT, data=json_pressure)
+    response_humidity     = requests.post(url=API_ENDPOINT, data=json_humidity)
+    response_acceleration = requests.post(url=API_ENDPOINT, data=json_acceleration)
+    response_orientation  = requests.post(url=API_ENDPOINT, data=json_orientation)
+    
+    #print(json_temperature)
+    #print(json_pressure)
+    #print(json_humidity)
+    #print(json_acceleration)
+    #print(json_orientation)
 
 
