7bb62fb3cd81340eca0fb3154247e9e0d8a1a1e7
Michael Lally <mikelally801@gmail.com>
2020-04-08 06:40:45 +0000

    removed print statements

--- hardware/SensorPi/main.py
+++ hardware/SensorPi/main.py
@@ -8,18 +8,16 @@ nyu_purple = (87, 46, 140)
 sense.show_message("MERCURY", text_colour=nyu_purple, scroll_speed=0.04)
 sense.clear()
 
-
 json_temperature  = temperature(sense)
 json_pressure     = pressure(sense)
 json_humidity     = humidity(sense)
 json_acceleration = acceleration(sense)
 json_orientation  = orientation(sense)
 
-
-print(json_temperature)
-print(json_pressure)
-print(json_humidity)
-print(json_acceleration)
-print(json_orientation)
+#print(json_temperature)
+#print(json_pressure)
+#print(json_humidity)
+#print(json_acceleration)
+#print(json_orientation)
 
 
