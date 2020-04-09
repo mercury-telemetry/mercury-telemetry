da399dbbc7d7f575b9957039a976bdda95af5057
ray310 <60367095+Ray310@users.noreply.github.com>
2020-04-09 02:48:26 -0400

    Removed no longer needed HTML divs and corresponding javascript references

--- mercury/static/mercury/js/sensor.js
+++ mercury/static/mercury/js/sensor.js
@@ -25,59 +25,22 @@ selectView displays the correct view of the "Add or Modify Sensors" page
 -if a user submits a new sensor which posts and reloads page, then the add new sensor view should remain
 */
 function selectView() {
-    var newSensorDiv = document.getElementById("add-new-sensor");
     var existingSensorDiv = document.getElementById("existing-sensors");
-    var editSensorsDiv = document.getElementById("edit-sensors");
-    var editSensorTypesDiv = document.getElementById("edit-sensor-types");
-
-    var modifyButton = document.getElementById("current-sensor-btn");
+    var newSensorDiv = document.getElementById("add-new-sensor");
+    var viewSensorsButton = document.getElementById("current-sensor-btn");
     var newSensorButton = document.getElementById("add-sensor-btn");
-    var editSensorsButton = document.getElementById("edit-sensors-btn");
-    var editSensorTypesButton = document.getElementById("edit-sensor-types-btn");
 
     if (sessionStorage.getItem('viewing') == 'addingNew'){
         newSensorDiv.style.display = "block";
         existingSensorDiv.style.display = 'none';
-        editSensorsDiv.style.display = "none";
-        editSensorTypesDiv.style.display = "none";
-
-        modifyButton.style.backgroundColor = 'var(--light-accent)';
+        viewSensorsButton.style.backgroundColor = 'var(--light-accent)';
         newSensorButton.style.backgroundColor = 'var(--green)';
-        editSensorsButton.style.backgroundColor = 'var(--light-accent)';
-        editSensorTypesButton.style.backgroundColor = 'var(--light-accent)';
     }
     else if (sessionStorage.getItem('viewing') == 'existing'){
         newSensorDiv.style.display = "none";
         existingSensorDiv.style.display = 'block';
-        editSensorsDiv.style.display = "none";
-        editSensorTypesDiv.style.display = "none";
-
-        modifyButton.style.backgroundColor = 'var(--green)';
+        viewSensorsButton.style.backgroundColor = 'var(--green)';
         newSensorButton.style.backgroundColor = 'var(--light-accent)';
-        editSensorsButton.style.backgroundColor = 'var(--light-accent)';
-        editSensorTypesButton.style.backgroundColor = 'var(--light-accent)';
-    }
-    else if (sessionStorage.getItem('viewing') == 'editingSensors'){
-        newSensorDiv.style.display = "none";
-        existingSensorDiv.style.display = 'none';
-        editSensorsDiv.style.display = "block";
-        editSensorTypesDiv.style.display = "none";
-
-        modifyButton.style.backgroundColor = 'var(--light-accent)';
-        newSensorButton.style.backgroundColor = 'var(--light-accent)';
-        editSensorsButton.style.backgroundColor = 'var(--green)';
-        editSensorTypesButton.style.backgroundColor = 'var(--light-accent)';
-    }
-    else if (sessionStorage.getItem('viewing') == 'editingSensorTypes'){
-        newSensorDiv.style.display = "none";
-        existingSensorDiv.style.display = 'none';
-        editSensorsDiv.style.display = "none";
-        editSensorTypesDiv.style.display = "block";
-
-        modifyButton.style.backgroundColor = 'var(--light-accent)';
-        newSensorButton.style.backgroundColor = 'var(--light-accent)';
-        editSensorsButton.style.backgroundColor = 'var(--light-accent)';
-        editSensorTypesButton.style.backgroundColor = 'var(--green)';
     }
     /*Added to prevent undesired behavior when opening sensors in new browser tab*/
     else {displayCurrentSensors()}
@@ -91,16 +54,6 @@ function displayCurrentSensors() {
 function displayAddNewSensor() {
   sessionStorage.setItem('viewing', 'addingNew');
   selectView()
-}
-
-function displayEditSensors() {
-    sessionStorage.setItem('viewing', 'editingSensors');
-    selectView()
-}
-
-function displayEditSensorTypes() {
-    sessionStorage.setItem('viewing', 'editingSensorTypes');
-    selectView()
 }
 
 function makeSensorEditable(sensor_name, offset) {
