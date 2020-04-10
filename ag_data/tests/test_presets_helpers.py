88cbda81ed8c6da9c1dc12a10d36577af64984e7
Jack XU <jackxujh@msn.com>
2020-04-10 18:38:55 -0400

    Format code

--- ag_data/tests/test_presets_helpers.py
+++ ag_data/tests/test_presets_helpers.py
@@ -23,7 +23,9 @@ class PresetsHelpersTest(TestCase):
             self.assertEqual(venue.name, reference["agVenueName"])
             self.assertEqual(venue.description, reference["agVenueDescription"])
             self.assertEqual(venue.latitude, Decimal(str(reference["agVenueLatitude"])))
-            self.assertEqual(venue.longitude, Decimal(str(reference["agVenueLongitude"])))
+            self.assertEqual(
+                venue.longitude, Decimal(str(reference["agVenueLongitude"]))
+            )
 
     def test_createVenueFromPresetAtIndex_out_of_range(self):
         totalTestVenues = len(presets.sample_venues)
@@ -37,29 +39,15 @@ class PresetsHelpersTest(TestCase):
         )
         self.assertEqual(str(e.exception), correct_exception_message)
 
-    def inactive_test_simulator_create_event(self):  # FIXME: reactivate this test
-        totalTestEvents = len(presets.event_presets)
+    def test_createVenueFromPresetAtIndex_multiple(self):
+        totalTestVenues = len(presets.sample_venues)
 
-        # test event creation for indices in range
-        for index in range(totalTestEvents):
-            self.sim.createAVenueFromPresets(index)
-            self.sim.createAnEventFromPresets(index)
-            current_event = presets.event_presets[index]
+        for index in range(totalTestVenues):
+            reference = presets.sample_venues[index]
 
-            event = AGEvent.objects.get(pk=self.sim.event.uuid)
-            self.assertEqual(event.name, current_event["agEventName"])
-            self.assertEqual((event.date), parse_datetime(current_event["agEventDate"]))
-            self.assertEqual(event.description, current_event["agEventDescription"])
+            venue = helpers.createVenueFromPresetAtIndex(index)
 
-        # test event creation for index out of range
-        with self.assertRaises(Exception) as e:
-            self.sim.createAnEventFromPresets(totalTestEvents)
-        correct_exception_message = (
-            "Cannot find requested event (index "
-            + str(totalTestEvents)
-            + ") from presets"
-        )
-        self.assertEqual(str(e.exception), correct_exception_message)
+        self.assertEqual(totalTestVenues, AGVenue.objects.count())
 
     def test_createSensorFromPresetAtIndex_in_range(self):
         totalTestSensors = len(presets.sample_sensors)
