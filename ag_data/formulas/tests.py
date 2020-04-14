2bde68e26bb01ed699f04471158eff96cf1f4cf0
Yonguk Jeong <yonguk@nyu.edu>
2020-04-14 00:43:48 -0400

    Update tests

--- ag_data/formulas/tests.py
+++ ag_data/formulas/tests.py
@@ -1,16 +1,17 @@
 from django.test import TestCase
 
 from ag_data.formulas.ingestion_engine import MeasurementIngestionEngine
-from ag_data.utilities import MeasurementExchange
-from django.utils import timezone
-from ag_data.models import AGEvent, AGVenue, AGSensorType, AGSensor, AGMeasurement
-from ag_data.formulas.library.system.mercury_formulas import (
-    fEmptyResult,
-    fMercurySimpleTemperatureSensor,
-    fMercuryDualTemperatureSensor,
-    fMercuryFlowSensor,
-)
+from ag_data.models import AGEvent, AGVenue, AGSensorType, AGSensor
+import ag_data.formulas.library.system.mercury_formulas as formulas
+
 import datetime
+
+
+def get_formula_id(f):
+    for fid, formula in formulas.processing_formulas.items():
+        if formula == f:
+            return fid
+    return None
 
 
 class MeasurementIngestionEngineTests(TestCase):
@@ -19,92 +20,71 @@ class MeasurementIngestionEngineTests(TestCase):
     def setUp(self):
         self.venue = AGVenue.objects.first()
         self.event = AGEvent.objects.first()
+        self.engine = MeasurementIngestionEngine(event=self.event)
+        self.timestamp = datetime.datetime.now()
 
-    def test_save_measurement(self):
-        sensor_type = AGSensorType.objects.create(
-            name="Coin Side Sensor",
-            processing_formula=0,
-            format={"reading": {"side": {"unit": "", "format": "bool"}}, "result": {}},
+    def test_save_measurement_flow(self):
+        formula_id = get_formula_id(formulas.flow_sensor)
+        sensor_type = AGSensorType.objects.filter(processing_formula=formula_id).first()
+        sensor = AGSensor.objects.create(name="Test Sensor", type_id=sensor_type)
+
+        result = self.engine.save_measurement(
+            sensor, self.timestamp, {"volumetricFlow": 2.0}
         )
-        sensor = AGSensor.objects.create(name="Test Sensor", type_id=sensor_type,)
+        self.assertEqual(result.timestamp, self.timestamp)
+        self.assertEqual(result.event_uuid, self.event)
+        self.assertEqual(result.sensor_id, sensor)
 
-        engine = MeasurementIngestionEngine()
+    def test_save_measurement_dual_temperature(self):
+        formula_id = get_formula_id(formulas.dual_temperature_sensor)
+        sensor_type = AGSensorType.objects.filter(processing_formula=formula_id).first()
+        sensor = AGSensor.objects.create(name="Test Sensor", type_id=sensor_type)
 
-        measurement = MeasurementExchange(
-            event=self.event,
-            timestamp=timezone.now(),
-            sensor=sensor,
-            reading={"side": True},
+        result = self.engine.save_measurement(
+            sensor, self.timestamp, {"internal": 2.0, "external": 1.0}
         )
-        result = engine.saveMeasurement(measurement)
-        self.assertEqual(result.timestamp, measurement.timestamp)
-        self.assertEqual(result.event_uuid, measurement.event)
-        self.assertEqual(result.sensor_id, measurement.sensor)
+        self.assertEqual(result.timestamp, self.timestamp)
+        self.assertEqual(result.event_uuid, self.event)
+        self.assertEqual(result.sensor_id, sensor)
+
+    def test_save_measurement_identity(self):
+        formula_id = get_formula_id(formulas.identity)
+        sensor_type = AGSensorType.objects.filter(processing_formula=formula_id).first()
+        sensor = AGSensor.objects.create(name="Test Sensor", type_id=sensor_type)
+
+        result = self.engine.save_measurement(
+            sensor, self.timestamp, {"internal": 2.0, "external": 1.0}
+        )
+        self.assertEqual(result.timestamp, self.timestamp)
+        self.assertEqual(result.event_uuid, self.event)
+        self.assertEqual(result.sensor_id, sensor)
 
 
 class FormulaTests(TestCase):
     fixtures = ["sample.json"]
 
-    def setUp(self):
-        event = AGEvent.objects.first()
-        sensor_type = AGSensorType.objects.first()
-        sensor = AGSensor.objects.create(name="Test Sensor", type_id=sensor_type,)
-        self.measurement = MeasurementExchange(
-            event=event,
-            timestamp=timezone.now(),
-            sensor=sensor,
-            reading={"side": True},
-        )
-
-    def test_empty_result(self):
-        self.measurement.reading = {
-            "side": True,
-        }
-        self.assertEqual(fEmptyResult(self.measurement), {})
-
-        self.measurement.reading = {}
-        self.assertEqual(fEmptyResult(self.measurement), {})
-
-        self.measurement.reading = {"random-key": 123, "random-key2": "abc"}
-        self.assertEqual(fEmptyResult(self.measurement), {})
+    def test_identity(self):
+        self.assertEqual(formulas.identity(), {})
+        self.assertEqual(formulas.identity(randomkey=10), {"randomkey": 10})
 
     def test_simple_temperature_sensor(self):
-        self.measurement.reading = {
-            "side": True,
-        }
-        self.assertEqual(fMercurySimpleTemperatureSensor(self.measurement), {})
-
-        self.measurement.reading = {}
-        self.assertEqual(fMercurySimpleTemperatureSensor(self.measurement), {})
-
-        self.measurement.reading = {"random-key": 123, "random-key2": "abc"}
-        self.assertEqual(fMercurySimpleTemperatureSensor(self.measurement), {})
+        self.assertEqual(
+            formulas.simple_temperature_sensor(temperature=10), {"temperature": 10}
+        )
 
     def test_dual_temperature_sensor(self):
-        self.measurement.reading = {"internal": 1, "external": 2}
-        res = fMercuryDualTemperatureSensor(self.measurement)
+        res = formulas.dual_temperature_sensor(internal=1, external=2)
         self.assertEqual(res, {"mean": 1.5, "diff": -1})
 
     def test_flow_sensor(self):
         now = datetime.datetime.now()
-
-        AGMeasurement.objects.filter(sensor_id=self.measurement.sensor.id).delete()
-
-        self.measurement.reading = {"volumetricFlow": 1}
-        self.measurement.timestamp = now
-        res = fMercuryFlowSensor(self.measurement)
-
+        res = formulas.flow_sensor(
+            prevGasLevel=None, prevTimestamp=None, volumetricFlow=1, timestamp=now
+        )
         self.assertEqual(res, {"gasLevel": 100})
 
-        AGMeasurement.objects.create(
-            timestamp=now,
-            event_uuid=self.measurement.event,
-            sensor_id=self.measurement.sensor,
-            value={"reading": self.measurement.reading, "result": res},
+        now, then = now + datetime.timedelta(seconds=10), now
+        res = formulas.flow_sensor(
+            prevGasLevel=100, prevTimestamp=then, volumetricFlow=2, timestamp=now
         )
-
-        self.measurement.reading = {"volumetricFlow": 2}
-        now = now + datetime.timedelta(seconds=10)
-        self.measurement.timestamp = now
-        res = fMercuryFlowSensor(self.measurement)
         self.assertEqual(res, {"gasLevel": 80})
