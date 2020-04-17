3528b4e2b76d073cd7b784afcb040e2c6a3b0510
Ziheng-Wang <bwg9623@gmail.com>
2020-04-17 10:25:43 +0800

    Update the ag_data from recently refactorized code

--- ag_data/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin  # noqa f401
-
-from .models import AGVenue, AGEvent, AGSensorType, AGSensor, AGMeasurement
-
-admin.site.register(AGVenue)
-admin.site.register(AGEvent)
-admin.site.register(AGSensorType)
-admin.site.register(AGSensor)
-admin.site.register(AGMeasurement)
