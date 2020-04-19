34b26fc0bf0f6cdfdf07a62ca67243a5d0f3770b
MichaelLally <mikelally801@gmail.com>
2020-04-19 00:10:32 -0500

    iterative change

--- hardware/gpsPi/gps-reader.py
+++ hardware/gpsPi/gps-reader.py
@@ -11,4 +11,28 @@ while True:
         pass
 
     nmeaSentence = gps.readline().split(",")
-    print(nmeaSentence)
+    nmeaType = nmeaSentence[0]
+
+    if nmeaType == "$GPRMC":
+        latitude_hours = float(nmeaSentence[3].split(".")[0])
+        latitude_minutes = float(nmeaSentence[3].split(".")[1])
+        longitude_hours = float(nmeaSentence[5].split(".")[0])
+        longitude_minutes = float(nmeaSentence[5].split(".")[1])
+
+        latitude_decimal = latitude_hours + latitude_minutes / 60
+        longitude_decimal = longitude_hours + longitude_minutes / 60
+
+        latitude_dir = nmeaSentence[4]
+        longitude_dir = nmeaSentence[6]
+
+        if latitude_dir == "S":
+            latitude_decimal = latitude_decimal * -1
+        if longitude_dir == "W":
+            longitude_decimal = longitude_decimal * -1
+        
+        print(latitude_decimal)
+        print(longitude_decimal)
+        print("")
+
+
+    # print(nmeaSentence)
