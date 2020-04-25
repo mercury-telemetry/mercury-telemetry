263faedb265742f178ed9294c56c0b7b48f03ff5
dandipietrantonio <dd2742@nyu.edu>
2020-04-25 16:42:10 -0400

    Revert "Made random ID generation more random"
    This reverts commit e58eab9a91c130aac7da9c8ad41403cc13012f0d.
    This commit was not needed since all tr id's are reset once the user hits update. The previous method of generating TR id's was less complicated and more effective.

--- mercury/static/mercury/js/sensor.js
+++ mercury/static/mercury/js/sensor.js
@@ -83,10 +83,11 @@ function addRow(table_name){
     var colCount = table.rows[0].cells.length;
     var newRow = table.insertRow(rowCount-1);
     var today = new Date();
-    var time = today.getHours() + today.getMinutes() + today.getSeconds() + today.getMilliseconds();
-    var newID = time * Math.random() * 1610612741; // large prime number
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+    var dateTime = date+' '+time;
     newRow.className = "sensor-fields-table-tr"
-    newRow.id = "sensor-fields-table-tr-".concat(newID) // need it to be unique so we can delete it if needed
+    newRow.id = "sensor-fields-table-tr-".concat(dateTime) // need it to be unique so we can delete it if needed
     for(var i=0; i<colCount; i++){
         var newCell = newRow.insertCell(i);
         if (i < 3) {
