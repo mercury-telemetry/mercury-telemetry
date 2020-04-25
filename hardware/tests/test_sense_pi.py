643f5a214991522e1d89e4b94f6437897351e4d6
Alex Biehl <ab7289@nyu.edu>
2020-04-25 18:57:01 -0400

    added hardware factory tests

--- hardware/tests/test_sense_pi.py
+++ hardware/tests/test_sense_pi.py
@@ -155,3 +155,216 @@ class SensePiTests(SimpleTestCase):
             self.assertIsInstance(sense.logging, Logger)
 
             self.assertDictEqual(sense.sensor_ids, expected_sensor_ids)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_temp(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+        mock_sense.return_value.get_temperature.return_value = "100"
+        mock_sense.return_value.get_pressure.return_value = "50"
+        mock_sense.return_value.get_humidity.return_value = "20"
+        mock_sense.return_value.get_accelerometer_raw.return_value = "20"
+        mock_sense.return_value.get_orientation.return_value = (1, 1, 1)
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("TEMPERATURE")
+
+            expected_data = {
+                "sensor_id": 1,
+                "values": {"temperature": "100"},
+                "date": date_str,
+            }
+
+            self.assertDictEqual(expected_data, data)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_pressure(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+        mock_sense.return_value.get_temperature.return_value = "100"
+        mock_sense.return_value.get_pressure.return_value = "50"
+        mock_sense.return_value.get_humidity.return_value = "20"
+        mock_sense.return_value.get_accelerometer_raw.return_value = "20"
+        mock_sense.return_value.get_orientation.return_value = (1, 1, 1)
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("PRESSURE")
+
+            expected_data = {
+                "sensor_id": 2,
+                "values": {"pressure": "50"},
+                "date": date_str,
+            }
+
+            self.assertDictEqual(expected_data, data)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_humidity(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+        mock_sense.return_value.get_temperature.return_value = "100"
+        mock_sense.return_value.get_pressure.return_value = "50"
+        mock_sense.return_value.get_humidity.return_value = "20"
+        mock_sense.return_value.get_accelerometer_raw.return_value = "20"
+        mock_sense.return_value.get_orientation.return_value = (1, 1, 1)
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("HUMIDITY")
+
+            expected_data = {
+                "sensor_id": 3,
+                "values": {"humidity": "20"},
+                "date": date_str,
+            }
+
+            self.assertDictEqual(expected_data, data)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_accel(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+        mock_sense.return_value.get_temperature.return_value = "100"
+        mock_sense.return_value.get_pressure.return_value = "50"
+        mock_sense.return_value.get_humidity.return_value = "20"
+        mock_sense.return_value.get_accelerometer_raw.return_value = "20"
+        mock_sense.return_value.get_orientation.return_value = (1, 1, 1)
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("ACCELERATION")
+
+            expected_data = {
+                "sensor_id": 4,
+                "values": {"acceleration": "20"},
+                "date": date_str,
+            }
+
+            self.assertDictEqual(expected_data, data)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_orientation(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+        mock_sense.return_value.get_temperature.return_value = "100"
+        mock_sense.return_value.get_pressure.return_value = "50"
+        mock_sense.return_value.get_humidity.return_value = "20"
+        mock_sense.return_value.get_accelerometer_raw.return_value = "20"
+        mock_sense.return_value.get_orientation.return_value = (1, 1, 1)
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("ORIENTATION")
+
+            expected_data = {
+                "sensor_id": 5,
+                "values": {"orientation": (1, 1, 1)},
+                "date": date_str,
+            }
+
+            self.assertDictEqual(expected_data, data)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_invalid_key(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("SOME_KEY")
+
+            expected_data = {}
+
+            self.assertDictEqual(expected_data, data)
+
+    @patch("hardware.SensorPi.sense_pi.date_str_with_current_timezone")
+    def test_factory_all(self, mock_date, mock_sense):
+
+        mock_show_message = MagicMock()
+        mock_clear = MagicMock()
+        mock_sense.return_value.show_message = mock_show_message
+        mock_sense.return_value.clear = mock_clear
+        mock_sense.return_value.get_temperature.return_value = "100"
+        mock_sense.return_value.get_pressure.return_value = "50"
+        mock_sense.return_value.get_humidity.return_value = "20"
+        mock_sense.return_value.get_accelerometer_raw.return_value = "20"
+        mock_sense.return_value.get_orientation.return_value = (1, 1, 1)
+
+        date_str = "example_date"
+        mock_date.return_value = date_str
+
+        with patch.dict(
+            os.environ,
+            {"SENSE_HAT_LOG_FILE": "logger.txt", "LOG_DIRECTORY": self.temp_dir.path},
+        ):
+            sense = SensePi()
+            data = sense.factory("ALL")
+
+            expected_data = {
+                "values": {
+                    "temperature": "100",
+                    "pressure": "50",
+                    "humidity": "20",
+                    "acceleration": "20",
+                    "orientation": (1, 1, 1),
+                },
+                "date": date_str,
+            }
+
+            self.assertDictEqual(expected_data, data)
