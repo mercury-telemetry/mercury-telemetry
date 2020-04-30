42c122f1ab102a77394606c786b80f7d1a4dd5e4
PrabhanshuAttri <contact@prabhanshu.com>
2020-04-30 17:44:50 -0400

    Added pip package install and minor change

--- hardware/setup/run_on_boot.sh
+++ hardware/setup/run_on_boot.sh
@@ -3,15 +3,11 @@ path=$(echo $PWD)
 cmd="python3 -m hardware.main"
 source_env="if [ -f hardware/env ]; then export \$(cat hardware/env | sed 's/#.*//g' | xargs); fi"
 
-echo $boot_file
-echo $path
-echo $cmd
 append_to_file () {
-  echo $1
   sed -i "\$i $1" $boot_file
 }
 
-if grep "$source_env" $boot_file
+if grep "$cmd" $boot_file
 then
   echo "Already set to run on boot"
 else
@@ -22,5 +18,6 @@ else
   append_to_file ")"
 fi
 
+sudo python3 -m pip install -r hardware/pi_requirements.txt
 sudo sudo systemctl status rc-local
 sudo systemctl daemon-reload
