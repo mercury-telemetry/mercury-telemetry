d8415adf857c610c4e618ba39915bffa39c23425
Jack XU <jackxujh@msn.com>
2020-05-01 00:32:25 -0400

    Use sensor serial in the measurement API

--- mercury/tests/test_measurement.py
+++ mercury/tests/test_measurement.py
@@ -1,5 +1,4 @@
 import json
-import uuid
 
 from django.test import TestCase
 from rest_framework.reverse import reverse
@@ -13,17 +12,13 @@ class TestMeasurement(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        sensor_uuid = uuid.uuid4()
-
         cls.test_measurement_data = {
-            "sensor_id": str(sensor_uuid),
+            "sensor_id": 1,
             "values": {"power": 2, "speed": 1},
             "date": "2020-03-11T19:20:00",
         }
         cls.sensor_type = AGSensorType.objects.first()
-        AGSensor.objects.create(
-            uuid=sensor_uuid, name="test sensor", type_id=cls.sensor_type
-        )
+        AGSensor.objects.create(serial=1, name="test sensor", type_id=cls.sensor_type)
         AGActiveEvent.objects.create(agevent=AGEvent.objects.first())
 
     def setUp(self):
